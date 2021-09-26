4227c7005c100543123446611ad981f213ea7a89
laintuv <89821223+laintuv@users.noreply.github.com>
2021-09-26 18:24:45 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -212,7 +212,7 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef QV2RAY_GUI
-    QApplication::setApplicationDisplayName("Qv2ray");
+    QApplication::setApplicationDisplayName("Qv2ray 2.7.1");
 #endif
 
 #ifdef QT_DEBUG
